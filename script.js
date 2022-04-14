e777421f4b198df484a92c3c1f5105351e5dea63
anepaz <anelisespaz@gmail.com>
2022-04-14 14:34:52 -0300

    create elements h2 and p with js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var toggle = document.getElementById('toggle');
+let toggle = document.getElementById('toggle');
 
 toggle.onclick = function(){
   toggle.classList.toggle('active')
 }
+
+let elementoPai = document.getElementById('text');
+
+let title = document.createElement('h2');
+title.textContent = "Descrição"
+elementoPai.appendChild(title);
+
+let p = document.createElement('p');
+p.textContent = "Parágrafo com a descrição do perfil"
+elementoPai.appendChild(p)
