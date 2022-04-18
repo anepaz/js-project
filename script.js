9e01531e1c5a6f99376009932ca422193bfb429a
anepaz <anelisespaz@gmail.com>
2022-04-18 10:12:37 -0300

    add click event for info button

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ toggle.onclick = function(){
   toggle.classList.toggle('active')
 }
 
-let elementoPai = document.getElementById('text');
+let infoBtn = document.getElementById('info');
 
-let title = document.createElement('h2');
-title.textContent = "Descrição"
-elementoPai.appendChild(title);
+infoBtn.onclick = function(){
+  infoBtn.classList.toggle('active')
+}
 
-let p = document.createElement('p');
-p.textContent = "Parágrafo com a descrição do perfil"
-elementoPai.appendChild(p)
+info.addEventListener('click', function(){
+
+  let elementoPai = document.getElementById('text');
+
+  let title = document.createElement('h2');
+  title.textContent = "Descrição"
+  elementoPai.appendChild(title);
+
+  let p = document.createElement('p');
+  p.textContent = "Parágrafo com a descrição do perfil"
+  elementoPai.appendChild(p);
+
+});
